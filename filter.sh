83c35e86b5dac9444edd43f330e6f2171ebb0b6c
Vitor George <vitor.george@gmail.com>
2013-12-09 19:05:47 -0200

    vacuum at sqlite file

--- filter.sh
+++ filter.sh
@@ -11,4 +11,5 @@ if [ "$closing_year" != "" ]; then
     echo "delete from deter where cast(year as integer) = "$opening_year" and cast(month as integer) < 8;" | spatialite data/deter.sqlite
     echo "delete from deter where cast(year as integer) = "$closing_year" and cast(month as integer) > 7;" | spatialite data/deter.sqlite
     echo "delete from deter where cast(year as integer) > "$closing_year";" | spatialite data/deter.sqlite
+    echo "drop table source; vacuum;" | spatialite data/deter.sqlite
 fi
